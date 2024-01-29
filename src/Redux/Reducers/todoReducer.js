ffe79bd85adc3fb4397c0f9acd8c7d97872b6098
SudipGhara19 <sudipghara135@gmail.com>
2024-01-30 01:55:10 +0530

    added reducer

---
+++ src/Redux/Reducers/todoReducer.js
@@ -0,0 +1,40 @@
+import { ADD_TODO, TOGGLE_TODO } from "../Actions/todoActions";
+
+//creating initial state
+const initialState = {
+    todos: []
+};
+
+
+//Reducer Function
+function todoReducer(state=initialState, action){
+
+    switch(action.type){
+
+        case ADD_TODO:
+            return{
+                ...state,
+                todos:[
+                    ...state,
+                    {
+                        text: action.text,
+                        completed: false
+                    }
+                ]
+            }
+
+        case TOGGLE_TODO:
+            return{
+                ...state,
+                todos: state.todos.map((todo, i)=> {
+                    if(i == action.index){
+                        todo.completed =! todo.completed;
+                    }
+                    return todo;
+                })
+            }
+        
+        default:
+            return state;
+    }
+}
